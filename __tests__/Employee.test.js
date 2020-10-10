c199e11d885513907a0440f25c0b7cff37a55180
ginomaglaque <ginomaglaque@gmail.com>
2020-10-10 08:14:22 -0700

    Test for get functions

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,11 +1,23 @@
 const Employee = require('../lib/Employee');
 
 test('creates a employee object', () => {
-    const employee = new Employee('Dave', 2, 'hello@world.com', 'Manager');
+    const employee = new Employee();
   
-    expect(employee.name).toBe('Dave');
+    expect(employee.name).toBe('');
     expect(employee.id).toEqual(expect.any(Number));
-    expect(employee.email).toBe('hello@world.com');
-    expect(employee.role).toBe('Manager');
+    expect(employee.email).toBe('');
+    expect(employee.role).toBe('');
 
 });
+
+test('get employee name, id, email, and role', () => {
+    const employee = new Employee();
+  
+    expect(employee.getName()).toHaveProperty('name');
+    expect(employee.getId()).toHaveProperty('id');
+    expect(employee.getEmail()).toHaveProperty('email');
+    expect(employee.getRole()).toHaveProperty('role');
+
+
+});
+
